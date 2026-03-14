b0b64fa957e65bb053bb97bd54a799d33b0bab16
dahanlapuk <itbamuhammadkamil@gmail.com>
2026-03-15 05:35:13 +0700

    Fix Debug: Rollup & ZIP Plugin Resolve

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['@capacitor-community/zip']
+  },
+  build: {
+    rollupOptions: {
+      external: ['@capacitor-community/zip']
+    }
+  }
 })
